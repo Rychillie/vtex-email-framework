9d7fa37e9be1537dad79bb5835ac88f19429804b
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2021-10-04 15:28:24 -0300

    Extra support for parent folder

--- webpack/server/app.js
+++ webpack/server/app.js
@@ -5,22 +5,27 @@ const cors = require('cors')
 const app = require('express')();
 const hbs = require('hbs');
 
+const useParentFolder = existsSync(root('../templates'));
+
 require(root('emails/helpers/vtex-helpers-v3.2.2'));
 hbs.registerHelper('clearText', (value) => value.replace(/\W+/g, ' '));
-hbs.registerPartials(root('emails/templates/partials'), function (err) { });
+hbs.registerPartials(useParentFolder ? root('../templates/partials') : root('emails/templates/partials'), function (err) { });
 
 function root(folders) {
     return resolve(...folders.split('/'));
 }
 
 function findModel(templateName) {
+    if (existsSync(root('../templates/defaults'))) {
+        if (readdirSync(root('../templates/defaults')).includes(templateName)) return templateName;
+    }
+
     if (existsSync(root('emails/templates/defaults'))) {
         if (readdirSync(root('emails/templates/defaults')).includes(templateName)) return templateName
     }
     return null
 }
-
-const viewFiles = readdirSync(root('emails/templates'));
+const viewFiles =  useParentFolder ? readdirSync(root('../templates')) : readdirSync(root('emails/templates'));
 const fileNames = viewFiles
     .filter(folder => /.hbs$/gi.test(folder))
     .map(template => {
@@ -31,7 +36,7 @@ const fileNames = viewFiles
     });
 
 app.use(cors());
-app.set('views', root('emails/templates'));
+app.set('views', useParentFolder ? root('../templates') : root('emails/templates'));
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
@@ -40,10 +45,20 @@ app.get('/', (req, res) => {
 
 fileNames.forEach(({ file }) => {
     app.get(`/${file}`, (req, res) => {
-        res.render(file, require(root(`emails/json/${file}.json`)))
+        res.render(file, useParentFolder ? require(root(`../json/${file}.json`)) : require(root(`emails/json/${file}.json`)))
     })
     app.get(`/templates/defaults/${file}`, (req, res) => {
-        res.render(root(`emails/templates/defaults/${file}`), require(root(`emails/json/${file}.json`)))
+        if (useParentFolder) {
+            res.render(
+                root(`../templates/defaults/${file}`), 
+                require(root(`../json/${file}.json`))
+            )
+        } else {
+            res.render(
+                root(`emails/templates/defaults/${file}`), 
+                require(root(`emails/json/${file}.json`))
+            )
+        }
     })
 });
 
